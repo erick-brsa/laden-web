4b239d0966cb82fd96a2ac4fa109bdd8e942655f
Perez-Mendez-Yael0804 <yperezm1900@alumno.ipn.mx>
2022-05-31 11:28:10 -0500

    Agregando la tabla de usuarios en el módulo del administrador

--- database/dbUsers.js
+++ database/dbUsers.js
@@ -1,75 +1,116 @@
-import bcrypt from 'bcryptjs';
+import bcrypt from "bcryptjs";
 import { prisma } from "./prisma";
 
 export const checkUserEmailPassword = async (email, password) => {
-    await prisma.$connect();
-    const user = await prisma.user.findUnique({
-        where: { email },
-        select: {
-            id: true,
-            email: true,
-            name: true,
-            password: true,
-            role: true,
-            image: true
-        },
-    });
-    await prisma.$disconnect();
+	await prisma.$connect();
+	const user = await prisma.user.findUnique({
+		where: { email },
+		select: {
+			id: true,
+			email: true,
+			name: true,
+			password: true,
+			role: true,
+			image: true,
+		},
+	});
+	await prisma.$disconnect();
 
-    if (!user) {
-        return null;
-    }
+	if (!user) {
+		return null;
+	}
 
-    if (!bcrypt.compareSync(password, user.password)) {
-        return null;
-    }
+	if (!bcrypt.compareSync(password, user.password)) {
+		return null;
+	}
 
-    const { id, role, name, image } = user;
-    return {
-        id,
-        email: email.toLocaleLowerCase(),
-        role,
-        name,
-        image,
-    };
-}
+	const { id, role, name, image } = user;
+	return {
+		id,
+		email: email.toLocaleLowerCase(),
+		role,
+		name,
+		image,
+	};
+};
 
 // Función para crear usuario mediante OAuth
 export const oAUthToDbUser = async (oAuthEmail, oAuthName) => {
-    await prisma.$connect();
-    const user = await prisma.user.findUnique({
-        where: { email: oAuthEmail },
-    });
+	await prisma.$connect();
+	const user = await prisma.user.findUnique({
+		where: { email: oAuthEmail },
+	});
 
-    if (user) {
-        await prisma.$disconnect();
-        const { id, name, email, role, image } = user;
-        return { id, name, email, role, image }
-    }
+	if (user) {
+		await prisma.$disconnect();
+		const { id, name, email, role, image } = user;
+		return { id, name, email, role, image };
+	}
 
-    const newUser = await prisma.user.create({
-        data: {
-            email: oAuthEmail,
-            name: oAuthName,
-            password: '',
-            image: 'https://ui-avatars.com/api/?size=500&background=047857&color=fff&bold=true&name=' + oAuthName.toLocaleLowerCase(),
-        }
-    });
+	const newUser = await prisma.user.create({
+		data: {
+			email: oAuthEmail,
+			name: oAuthName,
+			password: "",
+			image:
+				"https://ui-avatars.com/api/?size=500&background=047857&color=fff&bold=true&name=" +
+				oAuthName.toLocaleLowerCase(),
+		},
+	});
 
-    await prisma.$disconnect();
+	await prisma.$disconnect();
 
-    const { id, name, email, role } = newUser;
-    return { id, name, email, role, image };
-}
+	const { id, name, email, role } = newUser;
+	return { id, name, email, role, image };
+};
 
 // Obtener usuario por id
 export const getUserById = async (id) => {
+	await prisma.$connect();
+	const user = await prisma.user.findUnique({
+		select: {
+			id: true,
+			email: true,
+			name: true,
+			phoneNumber: true,
+			role: true,
+			image: true,
+		},
+		where: { id },
+	});
+	await prisma.$disconnect();
+
+	return user;
+};
+
+//Obtener usuario por role
+export const getClients = async () => {
+	await prisma.$connect();
+	const clients = await prisma.user.findMany({
+		where: { role: "cliente" },
+        select: {
+			id: true,
+			email: true,
+			name: true,
+			role: true,
+			image: true,
+		}
+	});
+	await prisma.$disconnect();
+
+	return clients;
+};
+
+export const getUsers = async () => {
     await prisma.$connect();
-    const user = await prisma.user.findUnique({
-        select: { id: true, email: true, name: true, phoneNumber: true, role: true, image: true},
-        where: { id },
-    });
+    const users = await prisma.user.findMany({
+        select: {
+			id: true,
+            name: true,
+            role: true,
+            email: true,
+        }
+    })
     await prisma.$disconnect();
-    
-    return user;
+    return users;
 }
